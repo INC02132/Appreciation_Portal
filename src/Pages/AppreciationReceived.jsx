89b952ce915deaca1ed0f9453d51db450736e5f4
prajojita-n <prajojita.nayak@incture.com>
2022-10-03 11:49:24 +0530

    received card updates

--- src/Pages/AppreciationReceived.jsx
+++ src/Pages/AppreciationReceived.jsx
@@ -11,17 +11,31 @@ import TextField from "@mui/material/TextField/TextField";
 import SearchIcon from "@mui/icons-material/Search";
 import IconButton from "@mui/material/IconButton";
 import { createRef, useCallback, useState } from "react";
+import axios from "axios";
 
-
-const handleCilck= () =>{
-  <img
-              height={"500px"}
-              src="../Assests/Slide1.jpg"
-              alt="Certificate"
-            />
-}
+const handleCilck = () => {
+  <img height={"500px"} src="../Assests/Slide1.jpg" alt="Certificate" />;
+};
 
 export default function AppreciationReceived() {
+  const [receivedCard, setReceivedCard] = useState(null);
+
+    const fetchData = async () => {
+      try{
+        let res = await axios.get(
+          "http://localhost:8080/appreciation/getAllAppreciation/10/1"
+        );
+        if(res.status===200) {
+          setReceivedCard(res.data.data)
+        }
+      } catch(error) {
+
+      }
+    }
+    React.useEffect(() => {
+      fetchData();
+    }, [])
+
   return (
     <Grid container>
       <Grid item md={5} style={{ padding: "16px" }}>
@@ -30,16 +44,18 @@ export default function AppreciationReceived() {
             width: "350px",
             height: "600px",
             borderRadius: "10px",
+            padding: "1em 0.2em",
           }}
         >
           <Typography
             sx={{
-              fontSize: "20px",
+              fontSize: "1rem",
               fontWeight: "500",
               textAlign: "center",
+              margin: "1em 0",
             }}
           >
-            Appreciation Received(4)
+            Appreciation Received
           </Typography>
           <Box
             sx={{
@@ -67,261 +83,87 @@ export default function AppreciationReceived() {
             </IconButton>
           </Box>
 
-          {/* <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions> */}
+          {
+            receivedCard?.map((item) => {
+              return <AppreciationCard cardData={item} />
+            })
+          }
 
-          <Card
-            elevation={8}
-            sx={{
-              width: "300px",
-              height: "107px",
-              borderRadius: "10px",
-              marginLeft: "25px",
-              marginTop: "20px",
-              color: "#002947",
-              backgroundColor: "#EAF2F9",
-              "& :hover": {
-                backgroundColor: "#002947",
-                color: "white",
-              },
-              "& :active": {
-                backgroundColor: "#002947",
-                color: "white",
-              },
-            }}
-          >
-            <div
-              style={{
-                height: "100%",
-              }}
-            >
-              <p
-                style={{
-                  textAlign: "right",
-                  fontWeight: "400px",
-                  fontSize: "0.9rem",
-                  fontFamily: "Roboto,Helvetica,Arial,sans-serif",
-                }}
-              >
-                26th September, 2022
-              </p>
-
-              <div style={{ marginLeft: "1rem", marginTop: "1.1rem" }}>
-                <p
-                  style={{
-                    fontWeight: "600px",
-                    fontSize: "1.3rem",
-                    fontFamily: "Roboto,Helvetica,Arial,sans-serif",
-                  }}
-                >
-                  Customer Centricity
-                </p>
-
-                <p
-                  style={{
-                    fontSize: "0.9rem",
-                    fontWeight: "400px",
-                    fontFamily: "Roboto,Helvetica,Arial,sans-serif",
-                    marginTop: "0.3rem",
-                  }}
-                >
-                  From: Silpa Shree Mohanty
-                </p>
-              </div>
-            </div>
-          </Card>
-          <Card
-            elevation={8}
-            sx={{
-              width: "300px",
-              height: "107px",
-              borderRadius: "10px",
-              marginLeft: "25px",
-              marginTop: "20px",
-              color: "#002947",
-              backgroundColor: "#EAF2F9",
-              "& :hover": {
-                backgroundColor: "#002947",
-                color: "white",
-              },
-              "& :active": {
-                backgroundColor: "#002947",
-                color: "white",
-              },
-            }}
-          >
-            <div
-              style={{
-                height: "100%",
-              }}
-            >
-              <p
-                style={{
-                  textAlign: "right",
-                  fontWeight: "400px",
-                  fontSize: "0.9rem",
-                  fontFamily: "Roboto,Helvetica,Arial,sans-serif",
-                }}
-              >
-                26th September, 2022
-              </p>
-
-              <div style={{ marginLeft: "1rem", marginTop: "1.1rem" }}>
-                <p
-                  style={{
-                    fontWeight: "600px",
-                    fontSize: "1.3rem",
-                    fontFamily: "Roboto,Helvetica,Arial,sans-serif",
-                  }}
-                >
-                  Customer Centricity
-                </p>
-
-                <p
-                  style={{
-                    fontSize: "0.9rem",
-                    fontWeight: "400px",
-                    fontFamily: "Roboto,Helvetica,Arial,sans-serif",
-                    marginTop: "0.3rem",
-                  }}
-                >
-                  From: Silpa Shree Mohanty
-                </p>
-              </div>
-            </div>
-          </Card>
-          <Card
-            elevation={8}
-            sx={{
-              width: "300px",
-              height: "107px",
-              borderRadius: "10px",
-              marginLeft: "25px",
-              marginTop: "20px",
-              color: "#002947",
-              backgroundColor: "#EAF2F9",
-              "& :hover": {
-                backgroundColor: "#002947",
-                color: "white",
-              },
-              "& :active": {
-                backgroundColor: "#002947",
-                color: "white",
-              },
-            }}
-          >
-            <div
-              style={{
-                height: "100%",
-              }}
-            >
-              <p
-                style={{
-                  textAlign: "right",
-                  fontWeight: "400px",
-                  fontSize: "0.9rem",
-                  fontFamily: "Roboto,Helvetica,Arial,sans-serif",
-                }}
-              >
-                26th September, 2022
-              </p>
-
-              <div style={{ marginLeft: "1rem", marginTop: "1.1rem" }}>
-                <p
-                  style={{
-                    fontWeight: "600px",
-                    fontSize: "1.3rem",
-                    fontFamily: "Roboto,Helvetica,Arial,sans-serif",
-                  }}
-                >
-                  Customer Centricity
-                </p>
-
-                <p
-                  style={{
-                    fontSize: "0.9rem",
-                    fontWeight: "400px",
-                    fontFamily: "Roboto,Helvetica,Arial,sans-serif",
-                    marginTop: "0.3rem",
-                  }}
-                >
-                  From: Silpa Shree Mohanty
-                </p>
-              </div>
-            </div>
-          </Card>
-          <Card onClick={handleCilck}
-            elevation={8}
-            sx={{
-              width: "300px",
-              height: "107px",
-              borderRadius: "10px",
-              marginLeft: "25px",
-              marginTop: "20px",
-              color: "#002947",
-              backgroundColor: "#EAF2F9",
-              "& :hover": {
-                backgroundColor: "#002947",
-                color: "white",
-              },
-              "& :active": {
-                backgroundColor: "#002947",
-                color: "white",
-              },
-            }}
-          >
-            <div
-              style={{
-                height: "100%",
-              }}
-            >
-              <p
-                style={{
-                  textAlign: "right",
-                  fontWeight: "400px",
-                  fontSize: "0.9rem",
-                  fontFamily: "Roboto,Helvetica,Arial,sans-serif",
-                }}
-              >
-                26th September, 2022
-              </p>
-
-              <div style={{ marginLeft: "1rem", marginTop: "1.1rem" }}>
-                <p
-                  style={{
-                    fontWeight: "600px",
-                    fontSize: "1.3rem",
-                    fontFamily: "Roboto,Helvetica,Arial,sans-serif",
-                  }}
-                >
-                  Customer Centricity
-                </p>
-
-                <p
-                  style={{
-                    fontSize: "0.9rem",
-                    fontWeight: "400px",
-                    fontFamily: "Roboto,Helvetica,Arial,sans-serif",
-                    marginTop: "0.3rem",
-                  }}
-                >
-                  From: Silpa Shree Mohanty
-                </p>
-              </div>
-            </div>
-          </Card>
+          
+          
         </Card>
       </Grid>
-          <Grid item md={7} style={{ padding: "16px" }}>
-    <img
-              height={"500px"}
-              src="https://images.unsplash.com/photo-1589330694653-ded6df03f754?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=916&q=80"
-              alt="Certificate"
-            />
+      <Grid item md={7} style={{ padding: "16px" }}>
+        <img
+          height={"500px"}
+          src="https://images.unsplash.com/photo-1589330694653-ded6df03f754?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=916&q=80"
+          alt="Certificate"
+        />
+      </Grid>
     </Grid>
-    </Grid> 
-
-   
   );
 }
+
+function AppreciationCard({cardData}) {
+  return (
+    <Card
+      onClick={handleCilck}
+      elevation={8}
+      sx={{
+        width: "300px",
+        height: "107px",
+        borderRadius: "10px",
+        marginLeft: "25px",
+        marginTop: "20px",
+        color: "#002947",
+        backgroundColor: "#EAF2F9",
+        "& :hover": {
+          backgroundColor: "#002947",
+          color: "white",
+        },
+        "& :active": {
+          backgroundColor: "#002947",
+          color: "white",
+        },
+      }}
+    >
+      <div
+        style={{
+          height: "100%",
+          padding: "1rem",
+        }}
+      >
+        <p
+          style={{
+            textAlign: "right",
+            fontSize: "0.8rem",
+          }}
+        >
+          {cardData?.date}
+        </p>
+
+        <div style={{ marginTop: "1.1rem" }}>
+          <p
+            style={{
+              fontWeight: "500",
+              fontSize: "1rem",
+            }}
+          >
+            {cardData?.template?.category}
+          </p>
+
+          <p
+            style={{
+              fontSize: "0.9rem",
+              fontWeight: "400",
+              marginTop: "0.1rem",
+            }}
+          >
+            From: {cardData?.sender?.firstName} {cardData?.sender?.lastName}
+          </p>
+        </div>
+      </div>
+    </Card>
+  );
+}
+  
