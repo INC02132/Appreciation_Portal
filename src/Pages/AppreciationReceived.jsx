5c131dd50136a8f6b48892c70def66da6ff008f7
prajojita-n <prajojita.nayak@incture.com>
2022-10-12 14:38:53 +0530

    Received Certificate

--- src/Pages/AppreciationReceived.jsx
+++ src/Pages/AppreciationReceived.jsx
@@ -6,24 +6,38 @@ import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
-import { CardHeader } from "@mui/material";
+import { Alert, CardHeader, Paper, Snackbar } from "@mui/material";
 import TextField from "@mui/material/TextField/TextField";
 import SearchIcon from "@mui/icons-material/Search";
 import IconButton from "@mui/material/IconButton";
 import { createRef, useCallback, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router";
+import { styled } from "@mui/system";
 import { useMsal } from "@azure/msal-react";
 import { TimeStampToDateString } from "../Utils/TimeStampToString";
 
-
+const StyledCertificate = styled(Paper)(({ theme }) => ({
+  boxShadow: "0 0 5px #000",
+  borderRadius: "0px",
+  overflow: "hidden",
+  position: "relative",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+}));
 
 export default function AppreciationReceived() {
   const { accounts } = useMsal();
- 
   const [receivedCard, setReceivedCard] = useState(null);
-
   const [searchText, setSearchText] = useState("");
+  const [toastMessage, setToastMessage] = useState(false);
+  const handleCloseToastMessage = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setToastMessage(false);
+  };
 
   const fetchData = async () => {
     try {
@@ -39,86 +53,173 @@ export default function AppreciationReceived() {
   };
 
   const [selectedCard, setSelectedCard] = useState(null);
- 
 
- 
- 
   React.useEffect(() => {
     fetchData();
-    
   }, []);
 
   return (
-    <Grid container>
-      <Grid item md={4} style={{ padding: "16px" }}>
-        <Card
+    <Box sx={{ height: "calc(100vh - 52px)" }}>
+      <Grid
+        sx={{
+          flexGrow: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+        }}
+        container
+        gap={8}
+      >
+        <Grid
+          item
+          xs={3}
           sx={{
-            width: "350px",
-            height: "600px",
-            borderRadius: "10px",
-            padding: "1em 0.2em",
-            cursor: "pointer"
+            height: "100%",
+            display: "flex",
+            flexDirection: "center",
+            justifyContent: "center",
+            alignItems: "center",
           }}
         >
-          <Typography
+          <Paper
+            className="wbScroll"
+            elevation={6}
             sx={{
-              fontSize: "1rem",
-              fontWeight: "500",
-              textAlign: "center",
-              margin: "1em 0",
+              height: "95%",
+              overflow: "auto",
             }}
           >
-            Appreciation Received
-          </Typography>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              border: 2,
-              marginLeft: "25px",
-              width: "300px",
-              borderColor: "#D9D9D9",
-              borderRadius: "7px",
-              padding: "0rem",
-            }}
-          >
-            <TextField
-              sx={{ paddingLeft: "0.5rem" }}
-              id="input-with-sx"
-              variant="standard"
-              placeholder="Search"
-              fullWidth
-              focused={false}
-              onChange={(e) => setSearchText(e.target.value)}
-              InputProps={{ disableUnderline: true }}
-            />
-            <IconButton disableRipple>
-              <SearchIcon />
-            </IconButton>
-          </Box>
+            <Grid container gap={1} sx={{ padding: "1em 1em" }}>
+              <Typography
+                sx={{
+                  fontSize: "1rem",
+                  fontWeight: "500",
+                  textAlign: "center",
+                  marginTop: "1em",
+                }}
+              >
+                Appreciation Received
+              </Typography>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  border: 1,
+                  width: "300px",
+                  borderColor: "#D9D9D9",
+                  borderRadius: "7px",
+                  padding: "0rem",
+                }}
+              >
+                <TextField
+                  sx={{ paddingLeft: "0.5rem" }}
+                  id="input-with-sx"
+                  variant="standard"
+                  placeholder="Search"
+                  fullWidth
+                  focused={false}
+                  onChange={(e) => setSearchText(e.target.value)}
+                  InputProps={{ disableUnderline: true }}
+                />
+                <IconButton disableRipple>
+                  <SearchIcon />
+                </IconButton>
+              </Box>
 
-          {receivedCard?.map((item) => {
-            return (searchText.toLowerCase().includes(item.template?.category?.toLowerCase())||
-            searchText.toLowerCase().includes( item.sender?.firstName?.toLowerCase)) &&
-             <AppreciationCard cardData={item} setSelectedCard={setSelectedCard} />;
-          })}
-        </Card>
+              {receivedCard?.map((item) => {
+                // return (
+                //   (searchText
+                //     .toLowerCase()
+                //     .includes(item.template?.category?.toLowerCase()) ||
+                //     searchText
+                //       .toLowerCase()
+                //       .includes(item.sender?.firstName?.toLowerCase)) && (
+                //     <AppreciationCard
+                //       cardData={item}
+                //       setSelectedCard={setSelectedCard}
+                //     />
+                //   )
+                // );
+                return (
+                  <AppreciationCard
+                      cardData={item}
+                      setSelectedCard={setSelectedCard}
+                    />
+                );
+              })}
+            </Grid>
+          </Paper>
+        </Grid>
+        <Grid
+          item
+          sx={{
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <StyledCertificate>
+            <Box
+              style={{
+                height: "100%",
+                width: "100%",
+                position: "absolute",
+                display: "flex",
+                gap: "2em",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+              }}
+            >
+              <Typography className="draggable">
+                {selectedCard?.template?.header} {accounts?.[0]?.name ?? " "},
+              </Typography>
+              <Typography className="draggable">
+                {selectedCard?.template?.basicMessage}{" "}
+                {selectedCard?.updatedMessage}
+              </Typography>
+              <Typography className="draggable">
+                {selectedCard?.template?.footer}
+                <br />
+                {selectedCard?.sender?.firstName}{" "}
+                {selectedCard?.sender?.lastName}
+              </Typography>
+            </Box>
+            <img
+              height={"500px"}
+              src={`data:image/png;base64,${selectedCard?.template?.templateFile}`}
+              alt="Certificate"
+            />
+          </StyledCertificate>
+          <Button variant="contained" sx={{ marginTop: "1rem" }}>
+            Download
+          </Button>
+        </Grid>
+
+        <Snackbar
+          open={toastMessage}
+          autoHideDuration={6000}
+          onClose={handleCloseToastMessage}
+        >
+          <Alert
+            onClose={handleCloseToastMessage}
+            severity="success"
+            sx={{ width: "100%", color: "#fff", backgroundColor: "#138019" }}
+          >
+            Value Card downloaded successfully!
+          </Alert>
+        </Snackbar>
       </Grid>
-      <Grid item md={8} style={{ padding: "16px", display: "flex", alignItems: "center" }}>
-        
-          <img src={`data:image/png;base64,${selectedCard?.template?.templateFile}`} 
-          height={"500px"}
-          />
-      </Grid>
-    </Grid>
+    </Box>
   );
-        };
+}
 
 function AppreciationCard({ cardData, setSelectedCard }) {
-  const handleClick=()=>{
-    setSelectedCard(cardData)
-  }
- 
+  const handleClick = () => {
+    setSelectedCard(cardData);
+  };
   return (
     <Card
       onClick={() => handleClick()}
@@ -127,8 +228,7 @@ function AppreciationCard({ cardData, setSelectedCard }) {
         width: "300px",
         height: "107px",
         borderRadius: "10px",
-        marginLeft: "25px",
-        marginTop: "20px",
+        marginTop: "10px",
         color: "#002947",
         backgroundColor: "#EAF2F9",
         "& :hover": {
@@ -139,6 +239,7 @@ function AppreciationCard({ cardData, setSelectedCard }) {
           backgroundColor: "#002947",
           color: "white",
         },
+        cursor: "pointer",
       }}
     >
       <div
