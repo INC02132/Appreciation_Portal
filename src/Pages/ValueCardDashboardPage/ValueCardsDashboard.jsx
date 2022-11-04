9373a1d6f7f4cf875d8ad3d164798404f225d86c
prajojita-n <prajojita.nayak@incture.com>
2022-11-04 14:56:16 +0530

    Css changes

--- src/Pages/ValueCardDashboardPage/ValueCardsDashboard.jsx
+++ src/Pages/ValueCardDashboardPage/ValueCardsDashboard.jsx
@@ -19,13 +19,14 @@ import { useSelector } from "react-redux";
 function ValueCards({ template }) {
   return (
     <div>
-      <Card sx={{ height: "10rem" }}>
+      <Card sx={{ height: "10rem", border: 1, borderColor: "#007AD4" }}>
         <Typography
           variant="h5"
           sx={{
             fontSize: "1rem",
             fontWeight: "500",
             margin: "1rem 1rem 0 1rem",
+            color: "#007AD4"
           }}
         >
           {template.category}
@@ -108,15 +109,15 @@ function ValueCardsDashboard({ setSelectedTemplate }) {
             </Typography> */}
             <Grid item>
 
-              <Paper sx={{ height: "100%", width: "280px", position: "relative" }}>
+              <Paper sx={{ height: "100%", width: "15.5rem", position: "relative" , display: "flex", justifyContent: "center", alignItems: "center"}}>
                 <EmojiEvents sx={{ color: "#ff900d", fontSize: "40px" }} style={{ transform: "rotate(-25deg)", position: "absolute", top: "-10px", left: "-10px" }} />
-                <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", padding: "0.5em" }} style={{ position: "absolute", top: "0", left: "0" }}>
+                <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", padding: "1em", paddingTop: "1.5rem" }} style={{ position: "absolute", top: "0", left: "0" }}>
 
-                  <Avatar sx={{ height: '40px', width: '40px', fontSize: '14px', marginBottom: "1rem" }}>{leaderBoardData?.[0]?.[1]?.firstName?.[0]}{leaderBoardData?.[0]?.[1]?.lastName?.[0]}</Avatar>
+                  <Avatar sx={{ height: '40px', width: '40px', fontSize: '14px', marginBottom: "1rem", backgroundColor: "#007AD4" }}>{leaderBoardData?.[0]?.[1]?.firstName?.[0]}{leaderBoardData?.[0]?.[1]?.lastName?.[0]}</Avatar>
                   <Box sx={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center" }}>
                     <Box sx={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center" }}>
                       {/* <Paper sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}> */}
-                      <Typography sx={{ fontSize: "30px", fontWeight: "bold" }}>
+                      <Typography sx={{ fontSize: "30px", fontWeight: "bold", color: "#007AD4" }}>
                         {leaderBoardData?.[0]?.[0]}
                       </Typography>
                       <Typography sx={{ fontSize: "14px" }}>
@@ -124,18 +125,18 @@ function ValueCardsDashboard({ setSelectedTemplate }) {
                       </Typography>
                       {/* </Paper> */}
                     </Box>
-                    <Typography>
-                      Name: {leaderBoardData?.[0]?.[1]?.firstName} {leaderBoardData?.[0]?.[1]?.lastName}
+                    <Typography sx={{ fontSize: "1.2rem", color: "#007AD4", fontWeight: "bold" }}>
+                      {leaderBoardData?.[0]?.[1]?.firstName} {leaderBoardData?.[0]?.[1]?.lastName}
                     </Typography>
-                    <Typography>
-                      Email: {leaderBoardData?.[0]?.[1]?.emailId}
+                    <Typography sx={{ fontSize: "1rem" }}>
+                      {leaderBoardData?.[0]?.[1]?.emailId}
                     </Typography>
                   </Box>
                 </Box>
               </Paper>
             </Grid>
             <Grid item flex={1} sx={{paddingRight: "1rem"}}>
-              <Typography sx={{ fontSize: "18px", fontWeight: "bold" }}>
+              <Typography sx={{ fontSize: "20px", fontWeight: "bold", color: "#002947" }}>
                 Leaderboard
               </Typography>
               <List sx={{ display: "flex", flexDirection: "column", gap: "10px", justifyContent: "stretch", alignContent: "stretch" }}>
@@ -146,22 +147,22 @@ function ValueCardsDashboard({ setSelectedTemplate }) {
                     if (index === 0) return (
                       <Paper elevation={0} sx={{ padding: "0.2em", height: "100%", display: "flex", justifyContent: "space-between", alignItems: "center" }}>
                         <Box sx={{width: "20%"}}>
-                          <Typography>
+                          <Typography sx={{fontWeight: "bold", color: "#002947"}}>
                             Rank
                           </Typography>
                         </Box>
                         <Box sx={{width: "25%"}}>
-                          <Typography>
+                          <Typography sx={{fontWeight: "bold", color: "#002947"}}>
                             Name
                           </Typography>
                         </Box>
                         <Box sx={{width: "35%"}}>
-                          <Typography>
+                          <Typography sx={{fontWeight: "bold", color: "#002947"}}>
                             Email
                           </Typography>
                         </Box>
                         <Box sx={{width: "20%", display: "flex", justifyContent: "center"}}>
-                          <Typography>
+                          <Typography sx={{fontWeight: "bold",  color: "#002947"}}>
                             Total Cards
                           </Typography>
                         </Box>
@@ -170,7 +171,7 @@ function ValueCardsDashboard({ setSelectedTemplate }) {
                     return (
                       <Paper sx={{ padding: "0.2em", height: "100%", display: "flex", justifyContent: "space-between", alignItems: "center" }}>
                         <Box sx={{width: "20%"}}>
-                        <Typography>
+                        <Typography sx={{fontWeight: "bold", color: "#007AD4"}}>
                           #{index+1}
                         </Typography>
                       </Box>
