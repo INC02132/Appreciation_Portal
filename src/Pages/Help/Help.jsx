2d59400be3606d6cf8fccd8fd8027242f6e377f1
prajojita-n <prajojita.nayak@incture.com>
2022-11-17 15:56:11 +0530

    ..

--- src/Pages/Help/Help.jsx
+++ src/Pages/Help/Help.jsx
@@ -1,4 +1,4 @@
-import { Typography } from '@mui/material'
+import { Paper, Typography } from '@mui/material'
 import React from 'react'
 import { useDispatch } from 'react-redux';
 import { setSelectedNavIndex } from '../../redux/reducers/appReducer';
@@ -9,11 +9,23 @@ function Help() {
   dispatch(setSelectedNavIndex(4));
 
   return (
-    <div style={{margin:"3rem", display: "flex", justifyContent: "center", alignContent: "center"}}>
-        <Typography sx={{fontSize: "1.5rem", color: "#002947", textAlign: "center"}}>
-        For any query related to value card, <br></br>please drop a mail at 
-        <b> askhr@incture.com</b>
-        </Typography>
+    <div style={{margin:"3rem", marginTop: "12rem", display: "flex", justifyContent: "center", alignContent: "center"}}>
+      <Paper elevation={4}
+            sx={{
+              backgroundColor: "#EAF2F9",
+              borderRadius: "10px",
+              width: "32rem",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              padding: "3rem 2rem 3rem 2rem",
+            }}
+          >
+          <Typography sx={{fontSize: "1.3rem", color: "#002947", textAlign: "center"}}>
+          For any query related to value cards, <br></br>please drop a mail at 
+          <b> askhr@incture.com</b>
+          </Typography>
+          </Paper>
         </div>
   )
 }
